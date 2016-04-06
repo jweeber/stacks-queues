225019281dcbc73d6239b0add8165a7a5222b5d6
Jessica <jessica.weeber@gmail.com>
2016-04-06 11:18:46 -0700

    stacks and queues completed

--- Queue.rb
+++ Queue.rb
@@ -1,13 +1,26 @@
+# FIFO
+
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
+  # take out
   def dequeue
+    # pop removes last element, but we put them in as the first element each time, so the last element is always the first one that was put in
+    @store.pop
   end
-  
+
+  # put in
   def enqueue(element)
+    @store.unshift(element)
   end
-  
+
   def size
+    @store.size
+  end
+
+  def empty?
+    size == 0
   end
 end
--- Stack.rb
+++ Stack.rb
@@ -2,13 +2,23 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def pop
+    @store.pop
   end
-  
+
   def push(element)
+    @store << element
   end
-  
+
   def size
+    @store.size
+    # or @store.length
   end
+
+  def empty?
+    size == 0
+    # @store.empty? also returns true/false
+  end
+
 end
